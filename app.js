e6f809cf94bfafa400e49ccb9195a3a9ba03ab9e
Ali Kurtulush <alikurtulus-92@hotmail.com>
2019-06-14 23:13:57 +0100

    Added type writter

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   console.log('Connected')
+  const typeWriter = document.getElementById('type-writer')
+  const dataText = typeWriter.getAttribute('data-text')
+  let count = 0
+  const dataTextLength = dataText.length
+
+  function setText(){
+    setTimeout(() => {
+      typeWriter.textContent +=dataText.charAt(count)
+      count++
+      if(count <=dataTextLength){
+        setText()
+      }
+    },100)
+
+  }
+  setText()
+
 
 
 

0ce196d25b5443c31efed8b197509075fc564f32
Ali Kurtulush <alikurtulus-92@hotmail.com>
2019-06-20 23:49:55 +0100

    Added project's link

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ document.addEventListener('DOMContentLoaded', () => {
   // const aboutLink = document.querySelector('.about-link')
   // const projectLink = document.querySelector('.project-link')
   // const contactLink= document.querySelector('.contact-link')
-  let mainNavLinks = document.querySelector('nav div.buttons a')
-  const mainSections = document.querySelectorAll('main section')
+  const mainNavLinks = document.querySelectorAll('nav div.buttons a')
+
   let lastId
   let cur = []
 
@@ -38,51 +38,8 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   })
 
-  // function smoothScroll(target, duration){
-  //   target = document.querySelector(target)
-  //   let targetPosition = target.getBoundingClientRect().top
-  //   let startPosition = window.pageYOffset
-  //   let distance = targetPosition - startPosition
-  //   let startTime = null
-  //   console.log(startPosition)
-  //   console.log(target)
-  //   console.log(targetPosition)
-  //   function animation(currentTime){
-  //     if(startTime === null) startTime = currentTime
-  //     const timeElapsed = currentTime - startTime
-  //     const run = ease(timeElapsed , startPosition, distance, duration)
-  //     window.scrollTo(0, run)
-  //     if(timeElapsed < duration) requestAnimationFrame(animation)
-  //   }
-  //   function ease(t, b , c , d){
-  //     t /= d / 2
-  //     if(t < 1) return c / 2 * t * t + b
-  //     t--
-  //     return - c / 2 * (t * (t - 2) - 1) + b
-  //   }
-  //   requestAnimationFrame(animation)
-  // }
 
   setText()
-
-  // aboutLink.addEventListener('click', function(e){
-  //   e.preventDefault()
-  //   // smoothScroll('.about-section', 1000)
-  //   scrollMove('about-section')
-  //
-  //
-  //
-  // })
-  // projectLink.addEventListener('click', function(e){
-  //   e.preventDefault()
-  //   scrollMove('project-section')
-  //   // smoothScroll('.project-section', 1000)
-  // })
-  // contactLink.addEventListener('click', function(e){
-  //   e.preventDefault()
-  //   // smoothScroll('.contact-section', 1000)
-  //   scrollMove('contact-section')
-  // })
 
 
 

7d0e3fd9bf80fc7d678d7b6ef28d525233fd28ab
Ali Kurtulush <alikurtulus-92@hotmail.com>
2019-06-21 15:19:28 +0100

    Changed the style of the contact section

--- app.js
+++ app.js
@@ -3,13 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('Connected')
   const typeWriter = document.getElementById('type-writer')
   const dataText = typeWriter.getAttribute('data-text')
-  // const aboutLink = document.querySelector('.about-link')
-  // const projectLink = document.querySelector('.project-link')
-  // const contactLink= document.querySelector('.contact-link')
   const mainNavLinks = document.querySelectorAll('nav div.buttons a')
-
-  let lastId
-  let cur = []
 
   let count = 0
   const dataTextLength = dataText.length
@@ -22,10 +16,10 @@ document.addEventListener('DOMContentLoaded', () => {
         setText()
       }
     },100)
-
   }
-
-  window.addEventListener('scroll', event => {
+  setText()
+  
+  window.addEventListener('scroll', function () {
     const fromTop = window.scrollY
     mainNavLinks.forEach(link => {
       const section = document.querySelector(link.hash)
@@ -39,7 +33,6 @@ document.addEventListener('DOMContentLoaded', () => {
   })
 
 
-  setText()
 
 
 
